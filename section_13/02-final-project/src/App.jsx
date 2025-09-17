8d9ac2c8a40ee0b73fa58304f323ba6c170a916a
haruhisayaguchi <hal.yaguchi@gmail.com>
2025-09-17 11:20:21 -0700

    #21 [modify] final project

---
+++ section_13/02-final-project/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+import Counter from './components/Counter/Counter.jsx';
+import Header from './components/Header.jsx';
+import { log } from './log.js';
+import ConfigureCounter from './components/ConfigureCounter.jsx';
+
+function App() {
+  log('<App /> rendered');
+
+  const [chosenCount, setChosenCount] = useState(0);
+
+  function handleSetChosenCount(count) {
+    setChosenCount(count);
+  }
+
+  return (
+    <>
+      <Header />
+      <main>
+        <ConfigureCounter onSet={handleSetChosenCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
+        <Counter initialCount={0} />
+      </main>
+    </>
+  );
+}
+
+export default App;

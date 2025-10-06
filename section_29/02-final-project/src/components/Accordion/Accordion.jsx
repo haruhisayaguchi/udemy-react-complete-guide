86fd9e8e817d70270c5c5d7e5451053c08921150
haruhisayaguchi <hal.yaguchi@gmail.com>
2025-10-05 19:02:06 -0700

    #53 [modify] final project

---
+++ section_29/02-final-project/src/components/Accordion/Accordion.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useState } from "react"
+import AccordionItem from "./AccordionItem";
+import AccordionTitle from "./AccordionTitle";
+import AccordionContent from "./AccordionContent";
+
+const AccordionContext = createContext();
+
+export function useAccordionContext() {
+	const ctx = useContext(AccordionContext);
+	if (!ctx) {
+		throw new Error('Accordion-related components must be wrapped by <Accordion>.');
+	}
+
+	return ctx;
+}
+
+export default function Accordion({ children, className }) {
+	const [openItemId, setOpenItemId] = useState();
+
+	function toggleItem(id) {
+		setOpenItemId(prev => prev === id ? null : id);
+	}
+
+	const contextValue = {
+		openItemId,
+		toggleItem
+	}
+
+	return (
+		<AccordionContext.Provider value={contextValue}>
+			<ul className={className}>
+				{children}
+			</ul>
+		</AccordionContext.Provider>
+	)
+}
+
+Accordion.Item = AccordionItem
+Accordion.Title = AccordionTitle
+Accordion.Content = AccordionContent

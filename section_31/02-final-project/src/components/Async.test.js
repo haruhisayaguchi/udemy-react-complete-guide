340683b141f0f6b3e5002ce818c7497965b6f84f
haruhisayaguchi <hal.yaguchi@gmail.com>
2025-10-07 15:42:36 -0700

    #57 [modify] final project

---
+++ section_31/02-final-project/src/components/Async.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react"
+import Async from "./Async"
+
+describe('Async component', () => {
+	test('renders posts if request succeeds', async () => {
+		// Arrange
+		window.fetch = jest.fn();
+		window.fetch.mockResolvedValueOnce({
+			json: async () => [
+				{ id: 'p1', title: 'First post' }
+			]
+		})
+		render(<Async />);
+		// Act
+
+		// Assert
+		const listitemElements = await screen.findAllByRole('listitem');
+		expect(listitemElements).not.toHaveLength(0);
+	})
+})
